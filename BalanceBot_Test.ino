de64ce76415dc7016915d2b0aae0fbf45da48199
David <6957239+delta-G@users.noreply.github.com>
2023-12-29 21:48:52 -0600

    not working yet

--- BalanceBot_Test.ino
+++ BalanceBot_Test.ino
@@ -23,8 +23,23 @@ BalanceBot_Test.ino  --  Test code for robot I'm building with @PickyBiker (foru
 
 #include "ICM_20948.h"
 
+#include <PID_v1.h>
+
+#include "WiFiS3.h"
+
+#include "arduino_secrets.h"
+#include "FormUpdatable.h"
+
 #define WIRE_PORT Wire1
 #define AD0_VAL 1
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;  // your network SSID (name)
+char pass[] = SECRET_PASS;  // your network password (use for WPA, or use as key for WEP)
+
+int status = WL_IDLE_STATUS;
+WiFiServer server(2080);
+IPAddress ipAddress(192, 168, 1, 81);
 
 const uint8_t rightStepPin = 2;
 const uint8_t rightDirPin = 5;
@@ -36,7 +51,16 @@ GPT_Stepper rightStepper(rightStepPin, rightDirPin, 17000.0, false);
 
 float speed = 10000.0;
 
+double Setpoint, Input, Output;
+double Kp = 2;
+double Ki = 0;
+double Kd = 0;
 
+FormUpdatableValue fuKp(Kp, "Kp");
+FormUpdatableValue fuKi(Ki, "Ki");
+FormUpdatableValue fuKd(Kd, "Kd");
+
+PID anglePID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 ICM_20948_I2C icm;
 icm_20948_DMP_data_t data;
@@ -45,6 +69,9 @@ void setup() {
   Serial.begin(115200);
   delay(500);
   Serial.println("\n\nStarting BalanceBot_Test.ino\n\n");
+  setupIMU();
+  startWiFi();
+  server.begin();
 
   leftStepper.init();
   rightStepper.init();
@@ -52,28 +79,38 @@ void setup() {
   WIRE_PORT.begin();
   WIRE_PORT.setClock(400000);
 
-  setupIMU();
+  
+  Setpoint = 0.0;
+  anglePID.SetOutputLimits(-34000, 34000);
+  anglePID.SetSampleTime(20);
+  Input = readPitch();
+  anglePID.SetMode(AUTOMATIC);
 }
 
 void loop() {
+  handleClient();
   controlLoop();
 }
 
-
-float pitchToSpeed(float pitch) {
-  float pmin = -45.0;
-  float pmax = 45.0;
-  float smin = -10000;
-  float smax = 10000;
-
-  return (pitch - pmin) * (smax - smin) / (pmax - pmin) + smin;
+void accelerate(double acc){
+  leftStepper.setAcceleration((acc>=0.0)? acc:-acc);
+  rightStepper.setAcceleration((acc>=0.0)? acc:-acc);
+  if(acc < 0){
+    leftStepper.setSpeed(-15000);
+    rightStepper.setSpeed(-15000);
+  } else if (acc > 0){
+    leftStepper.setSpeed(15000);
+    rightStepper.setSpeed(15000);
+  }
 }
+
 
 void controlLoop() {
   if (newData()) {
-    float speed = pitchToSpeed(readPitch());
-    leftStepper.setSpeed(speed);
-    rightStepper.setSpeed(speed);
+    anglePID.SetTunings(Kp, Ki, Kd);
+    Input = readPitch();
+    anglePID.Compute();
+    accelerate(Output);
   }
 }
 
@@ -179,4 +216,102 @@ double readPitch() {
   }
 
   return pitch;
+}
+
+
+void handleClient(){
+
+  WiFiClient client = server.available();
+
+  if (client) {
+    char currentLine[64] = {0};
+    uint8_t idx = 0;
+    while (client.connected()) {
+      delayMicroseconds(10);
+      if (client.available()) {
+        char c = client.read();
+        // Serial.write(c);
+        if (c == '\n') {
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (idx == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("<p>Use the forms below to set values</p>");
+            client.print("<br>");
+
+            // Show a list of forms 
+            FormUpdatable::listForms(&client);
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {  // if you got a newline, then clear currentLine:
+            // char buf[101];
+            // currentLine.toCharArray(buf, 100);
+            FormUpdatable::parse(currentLine);
+            currentLine[0] = 0;
+            idx = 0;
+          }
+        } else if (c != '\r') {
+          if (idx < 63) {
+            currentLine[idx] = c;
+            currentLine[++idx] = 0;
+          }
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+  }
+}
+
+
+void startWiFi() {
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true)
+      ;
+  }
+
+  if (WiFi.firmwareVersion() < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  WiFi.config(ipAddress);
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);  // print the network name (SSID);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+}
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
 }
